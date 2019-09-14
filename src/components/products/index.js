f577898d8422fa2a993f39b7f47175c799e09493
Marek Bareła <marek.barela@wp.pl>
2019-09-14 23:38:35 +0200

    basic projects component structure commited

--- src/components/products/index.js
+++ src/components/products/index.js
@@ -1,7 +1,16 @@
 import React from "react";
+import SectionHeader from "../section-header";
+import styles from "./index.module.css";
 
 const Products = () => {
-  return <main>Pruducts</main>;
+  const { container } = styles;
+  return (
+    <main>
+      <section className={container}>
+        <SectionHeader>Our Products</SectionHeader>
+      </section>
+    </main>
+  );
 };
 
 export default Products;
