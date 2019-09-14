395358fcf387d7e6a5127c3adbbb6b1f0a2a9e79
Marek Bareła <marek.barela@wp.pl>
2019-09-14 23:21:07 +0200

    hero component commited

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,7 +1,16 @@
 import React from "react";
+import styles from "./index.module.css";
 
 const Hero = () => {
-  return <section>Hero</section>;
+  const { hero, baner, header, button } = styles;
+  return (
+    <section className={hero}>
+      <div className={baner}>
+        <h1 className={header}>FURNITURE COLLECTION</h1>
+        <button className={button}>SHOP NOW</button>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
