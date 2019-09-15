9a0be0f3c9745e01d35cfb5606ba94ad34f148fc
Marek Bareła <marek.barela@wp.pl>
2019-09-15 22:17:16 +0200

    react icon commited

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Logo from "../header-logo";
 import ShoppingCart from "../header-cart";
+import reactImage from "../../images/react.png";
 import styles from "./index.module.css";
 
 const Header = () => {
-  const { header, wrapper } = styles;
+  const { header, wrapper, reactIcon } = styles;
   return (
     <header className={header}>
       <div className={wrapper}>
+        <img className={reactIcon} src={reactImage} alt="React" />
         <Logo />
         <ShoppingCart />
       </div>
