376fc3e6580e6386eafdf7535eb132918c574360
Marek Bareła <marek.barela@wp.pl>
2019-09-15 14:43:10 +0200

    basic redux setup commited

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app";
 import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import store from "./store/store";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 serviceWorker.unregister();
